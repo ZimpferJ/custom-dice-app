3b75d08b2f937109bbbf2ec2c771b17d3c634522
ZimpferJ <44075489+ZimpferJ@users.noreply.github.com>
2026-02-25 12:53:21 -0500

    Updated to have versioning

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,21 @@
+const CACHE_NAME = "dice-app-v2";
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open('dice-app').then(cache => {
             return cache.addAll([
